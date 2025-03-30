4223e905d6b73fbb8c15d3b00e9eb1e37408b9f5
Sourav Kumar Sahu <souravsahu70047@gmail.com>
2025-03-30 22:01:19 +0530

    Added Player and service

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,31 @@ io.on("connection", (socket) => {
     const { email, room } = data;
     emailToSocketId.set(email, socket.id);
     socketIdToEmail.set(socket.id, email);
-    // socket.join(data.room);
-    // socket.to(data.room).emit("user_joined", data.email);
+    socket.to(room).emit("user_joined", {email, id: socket.id});
+    socket.join(room);
     io.to(socket.id).emit("join_room", data);
   });
+
+  // socket.on("call_user", ({offer, to}) => {
+  //   console.log(offer, to);
+  //   io.to(to).emit("incoming_call", {
+  //     offer,
+  //     from: socket.id,
+  //   });
+  //   // const socketId = emailToSocketId.get(to);
+  //   // if (socketId) {
+  //   //   socket.to(socketId).emit("call_user", {
+  //   //     offer,
+  //   //     from: socket.id,
+  //   //   });
+  //   // }
+  // });
+
+  socket.on("call_user", ({ to, offer }) => {
+    console.log("Data from Icoming call", to, offer);
+    io.to(to).emit("incomming_call", { from: socket.id, offer });
+  });
+
   socket.on("send_message", (data) => {
     console.log(data);
     socket.to(data.room).emit("receive_message", data.message);
--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
+import { useSocket } from '../context/SocketProvider';
+import ReactPlayer from "react-player";
+import peer from '../service/peer';
+import { useNavigate } from 'react-router-dom';
 
 const Room = () => {
+  const socket = useSocket();
+  const [remoteSocketId, setRemoteSocketId] = React.useState(null);
+  const [myStream, setMyStream] = React.useState(null);
+  const navigate = useNavigate();
+
+  const handleUserJoined = useCallback((data) => {
+    const { email, id } = data;
+    console.log("User joined", email, id);
+    setRemoteSocketId(id);
+  }, []);
+
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    console.log("Stream", stream);
+    const offer = await peer.getOffer();
+    socket.emit("call_user", { to: remoteSocketId, offer });
+    setMyStream(stream);
+  }, [remoteSocketId, socket]);
+
+  const handleIncomingCall = useCallback(async ({ from, offer }) => {
+    console.log("Incoming call", offer, from);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("user_joined", handleUserJoined);
+    socket.on("incomming_call", handleIncomingCall);
+
+    return () => {
+      socket.off("user_joined", handleUserJoined);
+      socket.off("incomming_call", handleUserJoined);
+    };
+  }, [socket, handleUserJoined, handleIncomingCall]);
+
   return (
-    <div>Room</div>
+    <div>
+      <h1>Meeting Room</h1>
+      <button onClick={() => navigate("/")}>Home</button>
+      <h4>{remoteSocketId ? "Connected" : "No one in Room"}</h4>
+      {remoteSocketId && <button onClick={handleCallUser} >Call</button>}
+      {myStream && <ReactPlayer playing mute height="300px" width="100%" url={myStream} />}
+    </div>
   )
 }
 
